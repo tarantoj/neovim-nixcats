2d889299a903ff7ab34f3b6010d498318aa3be69
James Taranto <taranto.james@gmail.com>
2024-12-07 19:57:16 +1100

    show hidden files in oil

--- lua/myLuaConf/plugins/init.lua
+++ lua/myLuaConf/plugins/init.lua
@@ -24,6 +24,7 @@ if nixCats('general.extra') then
       'size',
       -- "mtime",
     },
+    view_options = { show_hidden = true },
     keymaps = {
       ['g?'] = 'actions.show_help',
       ['<CR>'] = 'actions.select',
