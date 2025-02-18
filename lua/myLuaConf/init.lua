3fad398938920ba395526bc3a1a8a32d0ae6fa5c
James Taranto <taranto.james@gmail.com>
2025-02-19 05:37:16 +1100

    remove deprecated handler

--- lua/myLuaConf/init.lua
+++ lua/myLuaConf/init.lua
@@ -1,19 +1,16 @@
-
 -- NOTE: various, non-plugin config
 require('myLuaConf.opts_and_keys')
 
--- NOTE: register the extra lze handlers because we want to use them.
-require("lze").register_handlers(require('lze.x'))
 -- NOTE: also add another one that makes enabling a spec for a category nicer
-require("lze").register_handlers(require('nixCatsUtils.lzUtils').for_cat)
+require('lze').register_handlers(require('nixCatsUtils.lzUtils').for_cat)
 
 -- NOTE: general plugins
-require("myLuaConf.plugins")
+require('myLuaConf.plugins')
 
 -- NOTE: obviously, more plugins, but more organized by what they do below
 
 -- I dont need to explain why this is called lsp right?
-require("myLuaConf.LSPs")
+require('myLuaConf.LSPs')
 
 -- NOTE: we even ask nixCats if we included our debug stuff in this setup! (we didnt)
 -- But we have a good base setup here as an example anyway!
