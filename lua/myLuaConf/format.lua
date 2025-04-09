72e48586b65a314ad3528561b2f56091369dc6ef
James Taranto <taranto.james@gmail.com>
2025-04-09 22:32:06 +1000

    prettier settings

--- lua/myLuaConf/format.lua
+++ lua/myLuaConf/format.lua
@@ -12,6 +12,25 @@ require('lze').load {
     after = function(plugin)
       local conform = require('conform')
 
+      local prettier_settings = {
+        require_cwd = true,
+
+        cwd = require('conform.util').root_file {
+          '.prettierrc',
+          '.prettierrc.json',
+          '.prettierrc.yml',
+          '.prettierrc.yaml',
+          '.prettierrc.json5',
+          '.prettierrc.js',
+          '.prettierrc.cjs',
+          '.prettierrc.mjs',
+          '.prettierrc.toml',
+          'prettier.config.js',
+          'prettier.config.cjs',
+          'prettier.config.mjs',
+        },
+      }
+
       conform.setup {
         formatters_by_ft = {
           -- NOTE: download some formatters in lspsAndRuntimeDeps
@@ -23,11 +42,20 @@ require('lze').load {
           -- python = { "isort", "black" },
           -- Use a sub-list to run only the first available formatter
           javascript = { 'prettier', 'prettierd', stop_after_first = true },
+          javascriptreact = { 'prettier', 'prettierd', stop_after_first = true },
+          typescriptreact = { 'prettier', 'prettierd', stop_after_first = true },
+          typescript = { 'prettier', 'prettierd', stop_after_first = true },
+          graphql = { 'prettier', 'prettierd', stop_after_first = true },
+          json = { 'prettier', 'prettierd', stop_after_first = true },
+          css = { 'prettier', 'prettierd', stop_after_first = true },
         },
         format_on_save = {
           -- These options will be passed to conform.format()
           timeout_ms = 500,
           lsp_format = 'fallback',
+        },
+        formatters = {
+          prettierd = prettier_settings,
         },
       }
 
