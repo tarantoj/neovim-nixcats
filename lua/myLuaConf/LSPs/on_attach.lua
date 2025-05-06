d529614f3001858ff27914dc75e436b00ce43a3d
James Taranto <taranto.james@gmail.com>
2025-05-06 21:38:17 +1000

    format

--- lua/myLuaConf/LSPs/on_attach.lua
+++ lua/myLuaConf/LSPs/on_attach.lua
@@ -19,10 +19,18 @@ return function(_, bufnr)
   -- because otherwise they would load telescope eagerly when this is defined.
   -- due to us using the on_require handler to make sure it is available.
   if nixCats('general.telescope') then
-    nmap('gr', function() require('telescope.builtin').lsp_references() end, '[G]oto [R]eferences')
-    nmap('gI', function() require('telescope.builtin').lsp_implementations() end, '[G]oto [I]mplementation')
-    nmap('<leader>ds', function() require('telescope.builtin').lsp_document_symbols() end, '[D]ocument [S]ymbols')
-    nmap('<leader>ws', function() require('telescope.builtin').lsp_dynamic_workspace_symbols() end, '[W]orkspace [S]ymbols')
+    nmap('gr', function()
+      require('telescope.builtin').lsp_references()
+    end, '[G]oto [R]eferences')
+    nmap('gI', function()
+      require('telescope.builtin').lsp_implementations()
+    end, '[G]oto [I]mplementation')
+    nmap('<leader>ds', function()
+      require('telescope.builtin').lsp_document_symbols()
+    end, '[D]ocument [S]ymbols')
+    nmap('<leader>ws', function()
+      require('telescope.builtin').lsp_dynamic_workspace_symbols()
+    end, '[W]orkspace [S]ymbols')
   end -- TODO: someone who knows the builtin versions of these to do instead help me out please.
 
   nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
@@ -43,5 +51,4 @@ return function(_, bufnr)
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
   end, { desc = 'Format current buffer with LSP' })
-
 end
